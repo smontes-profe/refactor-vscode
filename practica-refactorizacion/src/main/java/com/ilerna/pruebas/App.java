0d34cb772e0c9c2e1f6b8b908340e522883b51d3
Sergio MR <smontes@ilerna.com>
2026-01-29 14:31:18 +0100

    basic maven structure

---
+++ practica-refactorizacion/src/main/java/com/ilerna/pruebas/App.java
@@ -0,0 +1,13 @@
+package com.ilerna.pruebas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
